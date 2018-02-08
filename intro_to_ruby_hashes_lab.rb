3468eec629e114e02e5c162c406af9967f40a17c
Chely Ho <adept_inept@yahoo.com>
2018-02-08 17:45:44 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
 			:names => {
 				:reading_railroad => {},
 				:pennsylvania_railroad => {},
-				:b_and_o_railroad => {}
+				:b_and_o_railroad => {},
 				:shortline => {}
 			},
 			:rent_in_dollars => {
